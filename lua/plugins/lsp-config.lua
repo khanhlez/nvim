5a834c2e4243d4751efca51ead3cb427f8c01e5e
Khanh Le <khanhlezed@gmail.com>
2024-03-18 06:37:27 +0700

    add: lsp, none-ls and completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,13 +16,27 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.volar.setup({})
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      lspconfig.tsserver.setup({})
-      lspconfig.volar.setup({})
     end,
   },
 }
